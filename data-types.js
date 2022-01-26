714070acfeb8d8683479bec67a4c4b36aabcfb72
PaolaCrypto <paolaseverino@protonmail.com>
2022-01-25 20:20:33 -0500

    working with axios

--- data-types.js
+++ data-types.js
@@ -1,12 +1,8 @@
-const formData = new FormData();
-
-const btn = document.querySelector("#submit");
-const form = document.querySelector("#submit-form");
-const data = document.querySelector("#data-form");
-
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-});
+function displayData(response) {
+  console.log(response);
+}
 
 let dataForm = `https://frontend-take-home.fetchrewards.com/form`;
 console.log(dataForm);
+
+axios.get(dataForm).then(displayData);
